a2f89b9e1ae80d3a060e021fa6e5e141233173d0
Anthony Farmer <alf_eng@hotmail.com>
2023-06-13 14:31:39 -0700

    Implemented scroll box.

--- app/src/main/java/com/example/basiccodelab/PatchingActivity.kt
+++ app/src/main/java/com/example/basiccodelab/PatchingActivity.kt
@@ -3,6 +3,7 @@ package com.example.basiccodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
@@ -27,6 +29,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -56,7 +59,6 @@ class PatchingActivity : ComponentActivity() {
         super.onResume()
 
         // Fake progress
-
         var amount = 0.0f
         thread {
             while (amount < 1.0f) {
@@ -67,115 +69,135 @@ class PatchingActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun PatchingPreview() {
-    PatchScreen(PatchingActivity.progressAmount, PatchingActivity.progressStatus)
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun PatchingPreview() {
+        PatchScreen(progressAmount, progressStatus)
+    }
 
-@Composable
-private fun PatchScreen(amtLive: LiveData<Float>, statusLive: LiveData<String>)
-{
-    BasicCodelabTheme {
-        Box(contentAlignment = Alignment.TopCenter) {
-            Background()
-            Column(horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)) {
-                Title()
-                ProgressBar(amtLive, statusLive)
-                PatchDetails()
+    @Composable
+    private fun PatchScreen(amtLive: LiveData<Float>, statusLive: LiveData<String>)
+    {
+        val showLog = remember { mutableStateOf(false) }
+        BasicCodelabTheme {
+            Box(contentAlignment = Alignment.TopCenter) {
+                Background()
+                Column(horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)) {
+                    Title()
+                    ProgressBar(amtLive, statusLive)
+
+                    // Show log or cards
+                    if (showLog.value)
+                        PatchDetails()
+                    else
+                        PatchCards()
+                }
             }
         }
     }
-}
 
-@Composable
-private fun Background() {
-    Image(
-        modifier = Modifier.fillMaxSize(),
-        painter = painterResource(R.mipmap.background),
-        contentDescription = "background_image",
-        contentScale = ContentScale.FillBounds
-    )
-}
+    @Composable
+    private fun Background() {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.mipmap.background),
+            contentDescription = "background_image",
+            contentScale = ContentScale.FillBounds
+        )
+    }
 
-@Composable
-private fun Title() {
-    Text(text = "Updating game data",
-        color = MaterialTheme.colorScheme.inversePrimary,
-        modifier = Modifier
-            .padding(8.dp, 16.dp, 8.dp, 24.dp)
-            .wrapContentWidth(),
-        fontSize = 24.sp
-    )
-}
+    @Composable
+    private fun Title() {
+        Text(text = "Updating game data",
+            color = MaterialTheme.colorScheme.inversePrimary,
+            modifier = Modifier
+                .padding(8.dp, 16.dp, 8.dp, 24.dp)
+                .wrapContentWidth(),
+            fontSize = 24.sp
+        )
+    }
 
-@Composable
-private fun ProgressBar(amtLive: LiveData<Float>, statusLive: LiveData<String>) {
-    // Progress status
-    val status: String? by statusLive.observeAsState()
-    Text(text = (status?:""), color = MaterialTheme.colorScheme.inversePrimary,
-        modifier = Modifier
-            .padding(4.dp, 4.dp, 4.dp, 8.dp)
-            .wrapContentWidth()
-    )
+    @Composable
+    private fun ProgressBar(amtLive: LiveData<Float>, statusLive: LiveData<String>) {
+        // Progress status
+        val status: String? by statusLive.observeAsState()
+        Text(text = (status?:""), color = MaterialTheme.colorScheme.inversePrimary,
+            modifier = Modifier
+                .padding(4.dp, 4.dp, 4.dp, 8.dp)
+                .wrapContentWidth()
+        )
 
-    // Progress amount
-    val amount: Float? by amtLive.observeAsState()
-    LinearProgressIndicator((amount?:0.0f),
-        modifier = Modifier.padding(4.dp),
-        trackColor = MaterialTheme.colorScheme.primary,
-        color = MaterialTheme.colorScheme.inversePrimary
-    )
-}
+        // Progress amount
+        val amount: Float? by amtLive.observeAsState()
+        LinearProgressIndicator((amount?:0.0f),
+            modifier = Modifier.padding(4.dp),
+            trackColor = MaterialTheme.colorScheme.primary,
+            color = MaterialTheme.colorScheme.inversePrimary
+        )
+    }
 
-@Composable
-private fun PatchDetails() {
-    val showLog = remember { mutableStateOf(true) }
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    private fun PatchCards() {
+        val images = arrayOf(R.mipmap.page1, R.mipmap.page2,
+            R.mipmap.page3, R.mipmap.page4, R.mipmap.page5)
 
-    val buttColors = ButtonDefaults.buttonColors(
-        containerColor = MaterialTheme.colorScheme.primary,
-        contentColor = MaterialTheme.colorScheme.inversePrimary)
-
-    // Toggle Button
-    ElevatedButton(modifier = Modifier
-        .wrapContentSize()
-        .padding(8.dp, 64.dp, 8.dp, 16.dp),
-        colors = buttColors,
-        onClick = {
-            showLog.value = !showLog.value
+        Box(modifier = Modifier.padding(4.dp, 64.dp, 4.dp, 4.dp).wrapContentSize(),
+            contentAlignment = Alignment.Center) {
+            HorizontalPager(pageCount = 5) { page ->
+                Image(modifier = Modifier.alpha(0.75f).fillMaxSize(), contentDescription = "page_$page",
+                    painter = painterResource(images[page]), contentScale = ContentScale.FillBounds)
+            }
         }
-    ) {
-        Text(if (showLog.value) "Hide details" else "Show details", fontSize = 18.sp)
     }
 
-    // Log output
-    if (showLog.value) {
-        ShowLog()
+    @Composable
+    private fun PatchDetails() {
+        val showDetails = remember { mutableStateOf(true) }
+        val buttColors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.inversePrimary)
+
+        // Toggle Button
+        ElevatedButton(modifier = Modifier
+            .wrapContentSize()
+            .padding(8.dp, 64.dp, 8.dp, 16.dp),
+            colors = buttColors,
+            onClick = {
+                showDetails.value = !showDetails.value
+            }
+        ) {
+            Text(if (showDetails.value) "Hide details" else "Show details", fontSize = 18.sp)
+        }
+
+        // Log output
+        if (showDetails.value) {
+            ShowLog()
+        }
     }
-}
 
-@Composable
-private fun ShowLog() {
-    // Always scroll to the bottom when first displayed
-    val logState = rememberScrollState(0)
-    LaunchedEffect(Unit) { logState.scrollTo(logState.maxValue) }
+    @Composable
+    private fun ShowLog() {
+        // Always scroll to the bottom when first displayed
+        val logState = rememberScrollState(0)
+        LaunchedEffect(Unit) { logState.scrollTo(logState.maxValue) }
 
-    // Log text
-    Column(modifier = Modifier
-        .background(Color(0x88444455))
-        .padding(8.dp)
-        .fillMaxWidth()
-        .verticalScroll(logState)
-    ) {
-        repeat(40) {
-            Text("Line $it of tty... Line $it of tty... Line $it of tty...",
-                modifier = Modifier.padding(2.dp),
-                color = MaterialTheme.colorScheme.inversePrimary)
+        // Log text
+        Column(modifier = Modifier
+            .background(Color(0x88444455))
+            .padding(8.dp)
+            .fillMaxWidth()
+            .verticalScroll(logState)
+        ) {
+            repeat(40) {
+                Text("Line $it of tty... Line $it of tty... Line $it of tty...",
+                    modifier = Modifier.padding(2.dp),
+                    color = MaterialTheme.colorScheme.inversePrimary)
+            }
         }
     }
 }
